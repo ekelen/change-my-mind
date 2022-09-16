2d11358c12de2e16dd25d132f482dd7b3a0681a3
ekelen <erskelen@gmail.com>
2022-09-15 20:31:56 -0400

    change body font family

--- pages/index.js
+++ pages/index.js
@@ -136,7 +136,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Londrina+Sketch&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Londrina+Sketch&display=swap&family=Inconsolata&display=swap"
           rel="stylesheet"
         />
       </Head>
