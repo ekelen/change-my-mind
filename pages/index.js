6d2395992da4c747759da6127c047c5fe84d0609
ekelen <erskelen@gmail.com>
2022-10-10 22:34:49 -0400

    add about section

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { NOT_OARS_EXPLANATION, OARS, OARS_EXPLANATION } from "../data/dialogue";
 
 const Citation = () => {
   return (
-    <p style={{ fontSize: "small", paddingTop: "3rem" }}>
+    <p>
       Miller, W. R., & Rollnick, S. (2013).
       <span style={{ fontStyle: "italic" }}>
         {" "}
@@ -57,8 +57,8 @@ const Options = ({ options, onChooseResponse, hideHint, availableHints }) => {
       ))}
       {showingHint && (
         <div className={styles.optionHint}>
-          <button onClick={close} autoFocus={true}>
-            x
+          <button onClick={close} autoFocus={true} className={styles.closeX}>
+            <span>x</span>
           </button>
           <div className={styles.optionHintText}>
             {OARS_EXPLANATION[showingHint] ?? NOT_OARS_EXPLANATION[showingHint]}
@@ -158,23 +158,6 @@ const Story = ({ score, maxScore }) => {
 
 const PostGame = ({ children }) => {
   return <div className={styles.postGame}>{children}</div>;
-};
-
-const Header = ({ handleRestart, gameOver, gameWon, isDesktop }) => {
-  const buttonRef = useRef(null);
-  useEffect(() => {
-    if ((gameOver || gameWon) && buttonRef?.current) {
-      buttonRef.current.focus();
-    }
-  }, [gameOver, gameWon]);
-  return (
-    <div style={{ width: "1000px", display: "flex", alignItems: "baseline" }}>
-      <h1 style={{ marginRight: "auto" }}>I Will Eat You</h1>
-      <button onClick={handleRestart} ref={buttonRef} disabled={!isDesktop}>
-        Restart
-      </button>
-    </div>
-  );
 };
 
 const Dialogue = ({
@@ -306,6 +289,104 @@ const TextContainer = ({
           availableHints={availableHints}
         />
       )}
+    </div>
+  );
+};
+
+const About = ({ onClose }) => {
+  return (
+    <div className={styles.about}>
+      <button onClick={onClose} autoFocus={true} className={styles.closeX}>
+        <span>x</span>
+      </button>
+      <h1>About</h1>
+      <p>
+        This short text adventure is designed to illustrate some of the
+        principles of Motivational Interviewing, a style of communication
+        intended to help people (I&rsquo;ll call them <em>clients</em> here)
+        move towards a change they&rsquo;re ambivalent about making – if, and
+        how, they want to.
+      </p>
+      <p>
+        You do not need to read further to start playing. Just keep in mind the
+        &quot;Spirit of MI&quot;: <strong>Partnership</strong>,{" "}
+        <strong>Evocation</strong>, <strong>Acceptance</strong>, and{" "}
+        <strong>Compassion</strong>.
+      </p>
+      <p>
+        You can click the{" "}
+        <button disabled={true} className={styles.optionHintButton}>
+          ?
+        </button>{" "}
+        buttons that appear next to some options for tips.
+      </p>
+      <button onClick={onClose}>Close</button>
+      <h3>A little more...</h3>
+      <p>
+        Depending on a client&rsquo;s readiness to change, there are various
+        stages of communication in MI; in this game, we are in the{" "}
+        <em>evocation phase</em>, where we want to use our OARS:
+      </p>
+      <ul>
+        <li>
+          <strong>O</strong>pen-Ended Questions: <em>Open-ended questions</em>{" "}
+          to help the client explore their own thoughts and feelings.
+        </li>
+        <li>
+          <strong>A</strong>ffirmations: <em>Affirming</em> the client&rsquo;s
+          strengths, abilities, and efforts.
+        </li>
+        <li>
+          <strong>R</strong>eflections: <em>Reflecting</em> back to the client
+          what they have said, to allow both you and the client to hear what
+          they&rsquo;ve said in new words.
+        </li>
+        <li>
+          <strong>S</strong>ummaries: <em>Summarising</em> what the client has
+          shared with us, to link their ideas and confirm a shared
+          bigger-picture understanding of what we have learned from them.
+        </li>
+      </ul>
+      <p>
+        Please visit{" "}
+        <a
+          href="https://motivationalinterviewing.org/understanding-motivational-interviewing"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://motivationalinterviewing.org/understanding-motivational-interviewing
+        </a>{" "}
+        to learn more about MI.
+      </p>
+      <p>The only source used for this game is the OG textbook:</p>
+      <Citation />
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+const Header = ({ handleRestart, gameOver, gameWon, isDesktop }) => {
+  const buttonRef = useRef(null);
+  const [showAbout, setShowAbout] = useState(false);
+  useEffect(() => {
+    if ((gameOver || gameWon) && buttonRef?.current) {
+      buttonRef.current.focus();
+    }
+  }, [gameOver, gameWon]);
+  return (
+    <div style={{ width: "1000px", display: "flex", alignItems: "baseline" }}>
+      <h1 style={{ marginRight: "auto" }}>I Will Eat You</h1>
+      <button
+        onClick={() => setShowAbout(true)}
+        disabled={showAbout}
+        style={{ marginRight: "1rem" }}
+      >
+        About
+      </button>
+      <button onClick={handleRestart} ref={buttonRef} disabled={!isDesktop}>
+        Restart
+      </button>
+      {showAbout && <About onClose={() => setShowAbout(false)} />}
     </div>
   );
 };
